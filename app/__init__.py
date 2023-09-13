683c92ca85a3f306912bbcf1f000ba8d71737400
Yahya <yahya003jamal@emil>
2023-09-13 12:55:26 +0100

    new file

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,9 @@
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, jsonify, url_for
 from flask import render_template
 from .forms import LoginForm
-import json	
+import json
 import requests
+from suds.client import Client
 import secrets
 
 # Generate a secure random secret key
@@ -10,10 +11,9 @@ secret_key = secrets.token_hex(16)
 
 SALESFORCE_CONSUMER_KEY = '3MVG9DREgiBqN9WmkvM3yN3IVuL3AVGBSmf7SjpjZ3P6Za55OmT8i0VFDNK8t_vd9Dhj_oRqp.qqYZUMne.8N'
 SALESFORCE_CONSUMER_SECRET = 'ED6B772975C07C92B2F8EDDBC6C5F1E012C7ED30A54C04C00B27AB6C33FD4C27'
-SALESFORCE_REDIRECT_URI = 'https://www.google.com/'
+SALESFORCE_REDIRECT_URI = 'https://yahya003.pythonanywhere.com/oauthrp'
 
-SALESFORCE_API_VERSION =55
-
+SALESFORCE_API_VERSION =58
 
 app = Flask(__name__)
 
@@ -35,87 +35,203 @@ def login():
 
         # Set up URL based on Salesforce Connected App details
         oauth_url = f'{oauth_url}?response_type=code&client_id={SALESFORCE_CONSUMER_KEY}&redirect_uri={SALESFORCE_REDIRECT_URI}&state={environment}'
-        
+
         # Redirect to the login page
         return redirect(oauth_url)
 
     return render_template('index.html', form=login_form)
 
 @app.route('/oauthrp', methods=['GET', 'POST'])
-def oauth_response(request):
-	"""
-		Controller for the oauth_response page.
-	"""
+def oauth_response():
+    """
+    Controller for the oauth_response page.
+    """
 
-	# Default variables
-	error_exists = False
-	error_message = ''
-	username = ''
-	org_name = ''
-	org_id = ''
-	email = ''
+    # Default variables
+    error_exists = False
+    error_message = ''
+    username = ''
+    org_name = ''
+    org_id = ''
+    email = ''
 
-	# On page load
-	if request.GET:
+    # On page load
+    if request.method == 'GET':
 
-		# Get OAuth response  values
-		oauth_code = request.GET.get('code') 
-		environment = request.GET.get('state')
-		access_token = ''
-		instance_url = ''
+        # Get OAuth response values
+        oauth_code = request.args.get('code')
+        environment = request.args.get('state')
+        access_token = ''
+        instance_url = ''
 
-		if 'Sandbox' in environment:
-			login_url = 'https://test.salesforce.com'
-		else:
-			login_url = 'https://login.salesforce.com'
-		
-		# Log in to REST API to obtain access token
-		r = requests.post(login_url + '/services/oauth2/token', headers={ 'content-type':'application/x-www-form-urlencoded'}, data={'grant_type':'authorization_code','client_id':SALESFORCE_CONSUMER_KEY,'client_secret':SALESFORCE_CONSUMER_SECRET,'redirect_uri': SALESFORCE_REDIRECT_URI,'code': oauth_code})
-		
-		# Load JSON response
-		auth_response = json.loads(r.text)
+        if 'Sandbox' in environment:
+            login_url = 'https://test.salesforce.com'
+        else:
+            login_url = 'https://login.salesforce.com'
 
-		# If login error - return error for user
-		if 'error_description' in auth_response:
-			error_exists = True
-			error_message = auth_response['error_description']
+        # Log in to REST API to obtain access token
+        r = requests.post(login_url + '/services/oauth2/token', headers={'content-type': 'application/x-www-form-urlencoded'}, data={'grant_type': 'authorization_code', 'client_id': SALESFORCE_CONSUMER_KEY, 'client_secret': SALESFORCE_CONSUMER_SECRET, 'redirect_uri': SALESFORCE_REDIRECT_URI, 'code': oauth_code})
 
-		# Otherwise get session details
-		else:
-			access_token = auth_response['access_token']
-			instance_url = auth_response['instance_url']
-			user_id = auth_response['id'][-18:]
-			org_id = auth_response['id'][:-19]
-			org_id = org_id[-18:]
+        # Load JSON response
+        auth_response = json.loads(r.text)
 
-			# get username of the authenticated user
-			r = requests.get(instance_url + '/services/data/v' + str(SALESFORCE_API_VERSION) + '.0/sobjects/User/' + user_id + '?fields=Username,Email', headers={'Authorization': 'OAuth ' + access_token})
-			query_response = json.loads(r.text)
+        # If login error - return error for the user
+        if 'error_description' in auth_response:
+            error_exists = True
+            error_message = auth_response['error_description']
 
-			if 'Username' in query_response:
-				username = query_response['Username']
+        # Otherwise, get session details
+        else:
+            access_token = auth_response['access_token']
+            instance_url = auth_response['instance_url']
+            user_id = auth_response['id'][-18:]
+            org_id = auth_response['id'][:-19]
+            org_id = org_id[-18:]
 
-			if 'Email' in query_response:
-				email = query_response['Email']
+            # Get username of the authenticated user
+            r = requests.get(instance_url + '/services/data/v' + str(SALESFORCE_API_VERSION) + '.0/sobjects/User/' + user_id + '?fields=Username,Email', headers={'Authorization': 'OAuth ' + access_token})
+            query_response = json.loads(r.text)
 
-			# get the org name of the authenticated user
-			r = requests.get(instance_url + '/services/data/v' + str(SALESFORCE_API_VERSION) + '.0/sobjects/Organization/' + org_id + '?fields=Name', headers={'Authorization': 'OAuth ' + access_token})
-			
-			if 'Name' in json.loads(r.text):
-				org_name = json.loads(r.text)['Name']
+            if 'Username' in query_response:
+                username = query_response['Username']
 
-		login_form = LoginForm(initial={'environment': environment, 'access_token': access_token, 'instance_url': instance_url, 'org_id': org_id, 'username': username, 'org_name':org_name, 'email': email})	
+            if 'Email' in query_response:
+                email = query_response['Email']
 
-	# Run after user selects logout or get schema
-	
-	return render_template(
-        request, 
-        'oauthrp.html', 
-        {
-            'error': error_exists, 
-            'error_message': error_message, 
-            'username': username, 
-            'org_name': org_name, 
-            'login_form': login_form
+            # Get the org name of the authenticated user
+            r = requests.get(instance_url + '/services/data/v' + str(SALESFORCE_API_VERSION) + '.0/sobjects/Organization/' + org_id + '?fields=Name', headers={'Authorization': 'OAuth ' + access_token})
+
+            if 'Name' in json.loads(r.text):
+                org_name = json.loads(r.text)['Name']
+
+        login_form = LoginForm(environment=environment, access_token=access_token, instance_url=instance_url, org_id=org_id, username=username, org_name=org_name, email=email)
+
+        return render_template('oauthrp.html', error=error_exists, error_message=error_message,
+                               username=username, org_name=org_name, login_form=login_form)
+
+    # POST method to get objects
+    if request.method == 'POST' and 'get_metadata' in request.form:
+
+        login_form = LoginForm(request.form)
+        environment = login_form.environment.data
+        access_token = login_form.access_token.data
+        instance_url = login_form.instance_url.data
+        login_form = LoginForm(environment=environment, access_token=access_token, instance_url=instance_url)
+        #You can now use these variables as needed
+		#The list of standard objects that support custom fields
+		#https://www.salesforce.com/us/developer/docs/object_reference/Content/sforce_api_objects_custom_objects.html
+		# Define the list of supported standard objects
+        supported_standard_objects = [
+             'Account',
+             'Asset',
+             'Campaign',
+             'CampaignMember',
+             'Case',
+             'Contact',
+             'Contract',
+             'Event',
+             'Lead',
+             'Opportunity',
+             'OpportunityCompetitor',
+             'OpportunityLineItem',
+             'Order',
+             'OrderItem',
+             'Pricebook2',
+             'PricebookEntry',
+             'Product2',
+             'Quote',
+             'QuoteLineItem',
+             'Task',
+             'User',
+             'WorkOrder',
+             'WorkOrderLineItem',]
+        request_url = instance_url + '/services/data/v' + str(SALESFORCE_API_VERSION) + '.0/'
+        headers = {
+			'Accept': 'application/json',
+			'X-PrettyPrint': '1',
+			'Authorization': 'Bearer ' + access_token
 		}
-	)
+
+        custom_objects_infos = []
+        custom_objects = requests.get(request_url + 'sobjects', headers=headers).json()['sobjects']
+
+        for record in custom_objects:
+          custom_object_info = {
+            'label': record['label'],
+            'name': record['name'],
+          }
+          custom_objects_infos.append(custom_object_info)
+
+        # Now, custom_objects is a list of dictionaries representing CustomObject instances
+        return render_template('org_infos.html',
+                               login_form=login_form,
+                               custom_object =  custom_objects_infos
+                               )
+    if request.method == 'POST' and 'insert_object' in request.form:
+
+        login_form = LoginForm(request.form)
+        environment = login_form.environment.data
+        access_token = login_form.access_token.data
+        instance_url = login_form.instance_url.data
+        org_id= login_form.org_id.data
+        login_form = LoginForm(environment=environment, access_token=access_token, instance_url=instance_url)
+        metadata_client = Client('http://yahya003.pythonanywhere.com/static/metadata-52.xml')
+        metadata_url = instance_url + '/services/Soap/m/' +'52.0/' + org_id
+        session_header = metadata_client.factory.create("SessionHeader")
+        session_header.sessionId = access_token
+        metadata_client.set_options(location=metadata_url, soapheaders=session_header)
+
+        custom_object = metadata_client.factory.create("CustomObject")
+
+        custom_object.fullName = 'Flowsiti__c'
+        custom_object.label = 'Flowsiti'
+        custom_object.pluralLabel = 'Flowsitis'
+        custom_object.nameField =  metadata_client.factory.create("CustomField")
+        custom_object.nameField.type_x = 'Text'
+        custom_object.nameField.label = 'Flowsiti Record'
+        custom_object.deploymentStatus = 'Deployed'
+        custom_object.sharingModel = 'ReadWrite'
+        return jsonify(metadata_client)
+        """try:
+
+            result = metadata_client.service.createMetadata([custom_object])
+
+            if result[0].success:
+
+                page_response = {
+						'success': True,
+						'errorCode': None,
+						'message': 'Successfully created field.'
+					}
+
+            else:
+                    page_response = {
+						'success': False,
+						'errorCode': result[0].errors[0].statusCode,
+						'message': result[0].errors[0].message
+					}
+
+				# Return the POST response
+            return jsonify(page_response)
+
+        except Exception as ex:
+
+                page_response = {
+					'success': False,
+					'errorCode': 'Error building field metadata',
+					'message': ex
+				}
+
+                return jsonify(page_response)"""
+
+@app.route('/sclient', methods=['GET', 'POST'])
+def sclient(): 
+    if request.method == "GET":
+        url = Client('http://fieldcreator.herokuapp.com/static/metadata-52.xml')
+        print(url.factory.create("SessionHeader"))
+        return render_template('client.html',
+                               custom_object = url.factory.create("SessionHeader")
+                               )
+     
+
+
