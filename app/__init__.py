b6562ae90a156fd1ddb4d797873574e9b5e21800
yahya jamaldine <yahya003jamaldine@gmail.com>
2023-10-26 14:11:50 +0100

    field get

--- app/__init__.py
+++ app/__init__.py
@@ -196,8 +196,8 @@ def oauth_response():
         instance_url = login_form.instance_url.data
         org_id = login_form.org_id.data           
         custom_objects = requests.get(instance_url + '/services/data/v' + str(SALESFORCE_API_VERSION) + '.0/sobjects/Account/describe', headers={'Authorization': 'OAuth ' + access_token})
-        auth_response = json.loads(custom_objects.text)
-        
+        auth_response = json.loads(custom_objects.text)['fields']
+
         return auth_response
     
     if request.method == 'POST' and 'insert_object' in request.form:
